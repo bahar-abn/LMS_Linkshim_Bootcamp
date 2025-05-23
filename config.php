b3426d74385823224f78964bdf04424a9fc7ca69
bahar abolhasani <baharabolhasany2004@gmail.com>
2025-05-24 00:24:00 +0330

    Update config.php

--- config.php
+++ config.php
@@ -1,17 +1,11 @@
 <?php
+define('BASE_URL', '/lms-php-mvc/public');
+
 return [
     'db' => [
-        'dsn' => $_ENV['DB_DSN'],
-        'user' => $_ENV['DB_USER'],
-        'password' => $_ENV['DB_PASSWORD'],
+        'dsn' => 'mysql:host=localhost;port=3306;dbname=lms_db',
+        'user' => 'root',
+        'password' => ''
     ],
-    'userClass' => \app\models\User::class,
-    'baseUrl' => $_ENV['BASE_URL'] ?? 'http://localhost:8000',
-    'mailer' => [
-        'host' => $_ENV['MAILER_HOST'] ?? 'smtp.example.com',
-        'port' => $_ENV['MAILER_PORT'] ?? 587,
-        'username' => $_ENV['MAILER_USERNAME'] ?? '',
-        'password' => $_ENV['MAILER_PASSWORD'] ?? '',
-        'encryption' => $_ENV['MAILER_ENCRYPTION'] ?? 'tls',
-    ]
 ];
+
