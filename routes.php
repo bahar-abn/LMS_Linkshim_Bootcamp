b69a65441a0c33a83d2740d8ffc11381454eda85
Mahyas <MahyasGolparian@gmail.com>
2025-05-24 22:14:37 +0330

    Auth and Part2 integration

--- routes.php
+++ routes.php
@@ -1,35 +1,26 @@
 <?php
 
-//Courses:
-
 use controllers\CourseController;
-
-$router->get('/courses', [CourseController::class, 'index']);
-$router->get('/courses/create', [CourseController::class, 'create']);
-$router->post('/courses/store', [CourseController::class, 'store']);
-$router->get('/courses/{id}', [CourseController::class, 'show']);
-
-//Auth
-
-
-global $app;
-
 use controllers\AuthController;
 use core\Application;
 
+global $app;
+
+// Courses
+$app->router->get('/courses', [CourseController::class, 'index']);
+$app->router->get('/courses/create', [CourseController::class, 'create']);
+$app->router->post('/courses/store', [CourseController::class, 'store']);
+$app->router->get('/courses/{id}', [CourseController::class, 'show']);
+
+// Auth
 $app->router->get('/', [AuthController::class, 'login']);
 $app->router->get('/login', [AuthController::class, 'login']);
 $app->router->post('/login', [AuthController::class, 'loginPost']);
-
 $app->router->get('/register', [AuthController::class, 'register']);
 $app->router->post('/register', [AuthController::class, 'registerPost']);
-
 $app->router->get('/logout', [AuthController::class, 'logout']);
 
+// Dashboard
 $app->router->get('/dashboard', function() {
-    // You can replace this with a proper controller method later
     echo "Welcome to your dashboard!";
 });
-
-
-//...
